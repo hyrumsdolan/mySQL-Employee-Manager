e26f6cce3ef6bfc03e24334639a77f2ac42f83fa
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Air.local>
2024-01-21 19:02:37 -0700

    file structure"

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- DATABASE
+CREATE DATABASE IF NOT EXISTS CMS;
+USE CMS;
+
+-- Departments
+CREATE TABLE IF NOT EXISTS `departments` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(30) NOT NULL
+) ENGINE=InnoDB;
+
+-- Roles
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `title` VARCHAR(30) NOT NULL,
+  `salary` DECIMAL(10, 2) NOT NULL,
+  `department_id` INT, 
+  FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB;
+
+-- Employees
+CREATE TABLE IF NOT EXISTS `employees` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `first_name` VARCHAR(30) NOT NULL,
+  `last_name` VARCHAR(30) NOT NULL,
+  `role_id` INT, 
+  `manager_id` INT NULL,
+  FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE SET NULL,
+  FOREIGN KEY (`manager_id`) REFERENCES `employees` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB;
