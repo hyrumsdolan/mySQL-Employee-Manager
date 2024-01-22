8d97618b36fb2638fc86696461e8398ef8391ab6
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Air.local>
2024-01-21 22:06:23 -0700

    QOL Tweaks

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,7 +11,6 @@ INSERT INTO roles (title, salary, department_id) VALUES
 ('Deputy Headmistress', 55000, 1),
 ('Gamekeeper', 35000, 1),
 ('DADA Professor', 50000, 1),
-('Potions Master', 45000, 4),
 ('Herbology Researcher', 40000, 2),
 ('Charms Professor', 42000, 3);
 
@@ -20,6 +19,5 @@ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
 ('Minerva', 'McGonagall', 2, 1),
 ('Rubeus', 'Hagrid', 3, 2),
 ('Harry', 'Potter', 4, 2),
-('Horace', 'Slughorn', 5, 2),
-('Pomona', 'Sprout', 6, 1),
-('Filius', 'Flitwick', 7, 1);
+('Pomona', 'Sprout', 5, 1),
+('Filius', 'Flitwick', 6, 1);
